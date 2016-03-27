5403a5196a1ff3e83b5e25c4d9ade81ff562a688
goodspb <goodspb@qq.com>
2016-03-27 16:49:40 +0800

    fix bug: when logined, auto redirect to index

--- app/login/default.php
+++ app/login/default.php
@@ -1,4 +1,7 @@
-<?php include ROOT_PATH.'/app/public/header.php'; ?>
+<?php
+is_null(session("username")) or redirect('/index');
+include ROOT_PATH.'/app/public/header.php';
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
